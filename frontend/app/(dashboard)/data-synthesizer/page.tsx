f8f1668ed01d751b6b556d58309f06527c775ab6
pvtmanav18 <pvtmanav18@gmail.com>
2026-04-26 23:35:56 +0530

    Update page.tsx

--- frontend/app/(dashboard)/data-synthesizer/page.tsx
+++ frontend/app/(dashboard)/data-synthesizer/page.tsx
@@ -40,7 +40,7 @@ export default function UploadPage() {
       const formData = new FormData();
       formData.append("file", file);
 
-      const response = await fetch("http://127.0.0.1:8000/analyze", {
+      const response = await fetch("http://127.0.0.1:8000/synthesize", {
         method: "POST",
         body: formData,
       });
@@ -68,7 +68,7 @@ export default function UploadPage() {
       formData.append("file", file);
 
       const response = await fetch(
-        "http://127.0.0.1:8000/download-cleaned",
+        "http://127.0.0.1:8000/download-synthesized",
         {
           method: "POST",
           body: formData,
